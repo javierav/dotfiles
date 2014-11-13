c5b147aeff402c57cca990c2bb9d3637f6c1c8ae
Javier Aranda <javier.aranda.varo@gmail.com>
2014-11-14 00:54:41 +0100

    use colors in the installer prompt

--- install.sh
+++ install.sh
@@ -1,36 +1,47 @@
 #!/usr/bin/env bash
 
-echo "==============================="
+# colors
+OFF='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+
+# title
+echo -e "$PURPLE==============================="
 echo " ARANDAIO'S DOTFILES INSTALLER "
-echo "==============================="
+echo -e "===============================$OFF\n"
 
 # check dependencies
 if ! hash realpath 2> /dev/null; then
-  echo "Please install the 'realpath' command!"
+  echo -e "${RED}Please install the 'realpath' command!"
   exit 1
 fi
 
+# opts
 while getopts f opts; do
  case ${opts} in
   f) FORCE=true ;;
  esac
 done
 
+# install dotfiles
 while read -r -u 3 file; do
   if [ -e "$HOME/.dotignore" ] && grep -Fxq "$file" "$HOME/.dotignore"; then
-    echo "\$HOME/.$file included in .dotignore file. Skipped!"
+    echo -e "$BLUE\$HOME/.$file included in .dotignore file. Skipped!$OFF"
     continue
   fi
 
   if [ -e "$HOME/.$file" ] && [ -z "$FORCE" ]; then
-    read -e -p "\$HOME/.$file exists, overwrite (y/n)?: " -n 1 answer
+    read -e -p $'\033[0;33m'"\$HOME/.$file exists, overwrite (y/n)?: "$'\033[0m' -n 1 answer
 
     if [[ $answer = [nN] ]]; then
-      echo "\$HOME/.$file skipped!"
+      echo -e "$BLUE\$HOME/.$file skipped!$OFF"
       continue
     fi
   fi
 
   cp -f $(realpath $(dirname $0))/$file $HOME/.$file
-  echo "\$HOME/.$file installed!"
+  echo -e "$GREEN\$HOME/.$file installed!$OFF"
 done 3< $(realpath $(dirname $0))/_files
