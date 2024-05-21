2571eb862b79ffef345ef1582ecea0f45e4d1412
Javier Aranda <aranda@hey.com>
2024-05-21 18:38:53 +0200

    Improve install script

--- install.sh
+++ install.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
-# colors
-OFF='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
+# files
+FILES=(
+  "link:asdfrc:.asdfrc"
+  "link:bash_profile:.bash_profile"
+  "link:bashrc:.bashrc"
+  "link:default-gems:.default-gems"
+  "link:gemrc:.gemrc"
+  "link:gitconfig:.gitconfig"
+  "link:gitignore:.gitignore"
+)
 
-usage() {
+# paths
+SOURCE_PATH="$HOME/.dotfiles"
+TARGET_PATH="$HOME"
+
+print::usage() {
   cat << EOF
 
   Usage: $0 [options]
@@ -31,157 +38,211 @@ usage() {
 EOF
 }
 
-# check dependencies
-dependencies=(realpath md5sum awk)
+print::message() {
+  local color="$1"
+  local message="$2"
+  local off='\033[0m'
+  local red='\033[0;31m'
+  local green='\033[0;32m'
+  local yellow='\033[0;33m'
+  local blue='\033[0;34m'
+  local purple='\033[0;35m'
 
-for dep in "${dependencies[@]}"; do
-  if ! hash "$dep" 2> /dev/null; then
-    echo -e "${RED}Error. Please install the '$dep' command!$OFF\n"
-    exit 1
+  echo -e "${!color}$message$off"
+}
+
+check::dependencies() {
+  local dependencies=(md5sum awk)
+
+  for dep in "${dependencies[@]}"; do
+    if ! hash "$dep" 2> /dev/null; then
+      print::message red "Error. Please install the '$dep' command!"
+      exit 1
+    fi
+  done
+}
+
+options::parse() {
+  while getopts "fo:s:i:pah" opts; do
+   case ${opts} in
+    f)
+      FORCE="yes"
+      ;;
+    o)
+      ONLY+=("$OPTARG")
+      ;;
+    p)
+      PRETEND="yes"
+      ;;
+    s)
+      SKIP+=("$OPTARG")
+      ;;
+    a)
+      ASK="yes"
+      ;;
+    i)
+      TARGET_PATH="$OPTARG"
+      ;;
+    h)
+      print::usage
+      exit 1
+   esac
+  done
+}
+
+process::dotfiles() {
+  for file in "${FILES[@]}"; do
+    IFS=":" read -r operation source_file target_file <<< "$file"
+    process::file "$operation" "$source_file" "$target_file"
+  done
+}
+
+process::file() {
+  local operation="$1"
+  local source_file="$2"
+  local target_file="$3"
+
+  check::dotignore "$source_file" "$target_file" || return
+  check::only "$source_file" "$target_file" || return
+  check::skip "$source_file" "$target_file" || return
+  install::ask "$target_file" || return
+  install::diff "$source_file" "$target_file" || return
+  install::perform "$operation" "$source_file" "$target_file"
+}
+
+check::dotignore() {
+  local source_file="$1"
+  local target_file="$2"
+
+  if [ -e "$TARGET_PATH/.dotignore" ] && grep -Fxq "$source_file" "$TARGET_PATH/.dotignore"; then
+    print::message blue "$TARGET_PATH/$target_file skipped! (included in .dotignore)"
+    return 1
   fi
-done
+}
 
-# current path
-current_path=$(realpath $(dirname $0))
-
-# opts
-while getopts "fo:s:i:pah" opts; do
- case ${opts} in
-  f)
-    FORCE="yes"
-    ;;
-  o)
-    ONLY+=("$OPTARG")
-    ;;
-  p)
-    PRETEND="yes"
-    ;;
-  s)
-    SKIP+=("$OPTARG")
-    ;;
-  a)
-    ASK="yes"
-    ;;
-  i)
-    INSTALLDIR+=("$OPTARG")
-    ;;
-  h)
-    usage
-    exit 1
- esac
-done
-
-
-if [ -z "$INSTALLDIR" ]; then
-  INSTALLDIR="$HOME"
-fi
-
-# install dotfiles
-while read -r -u 3 file; do
-  # skip due to dotignore
-  if [ -e "$INSTALLDIR/.dotignore" ] && grep -Fxq "$file" "$INSTALLDIR/.dotignore"; then
-    echo -e "$BLUE$INSTALLDIR/.$file skipped! (included in .dotignore)$OFF"
-    continue
-  fi
-
-  # skip due to only option
-  included=0
+check::only() {
+  local source_file="$1"
+  local target_file="$2"
+  local included=0
 
   for o in "${ONLY[@]}"; do
-    if [ "$o" == "$file" ]; then
+    if [ "$o" == "$source_file" ]; then
       included=1
     fi
   done
 
   if [ $included -eq 0 ] && [ ${#ONLY[@]} -gt 0 ]; then
-    echo -e "$BLUE$INSTALLDIR/.$file skipped!$OFF"
-    continue
+    print::message blue "$TARGET_PATH/$target_file skipped! (not included in the only list)"
+    return 1
   fi
+}
 
-  # skip option
+check::skip() {
+  local source_file="$1"
+  local target_file="$2"
+
   for s in "${SKIP[@]}"; do
-    if [ "$s" == "$file" ]; then
-      continue 2
+    if [ "$s" == "$source_file" ]; then
+      print::message blue "$TARGET_PATH/$target_file skipped! (included in the skip list)"
+      return 1
     fi
   done
+}
 
-  # ask for install
+install::ask() {
+  local target_file="$1"
+
   if [ -n "$ASK" ]; then
     while true; do
       # print question
-      read -e -p $'\033[0;33m'"Do you want to install $INSTALLDIR/.$file file? [y,n,h]: "$'\033[0m' -n 1 answer
+      read -e -p $'\033[0;33m'"Do you want to install $TARGET_PATH/$target_file file? [y,n,h]: "$'\033[0m' -n 1 answer
 
-      # print help
       if [[ $answer = [hH] ]]; then
+        # print help
         echo -e "y - install file"
         echo -e "n - not install file"
         echo -e "h - print this help"
-        continue
-      fi
-
-      # install
-      if [[ $answer = [yY] ]]; then
-        break
-      fi
-
-      # skip
-      if [[ $answer = [nN] ]]; then
-        echo -e "$BLUE$INSTALLDIR/.$file skipped!$OFF"
-        continue 2
+      elif [[ $answer = [yY] ]]; then
+        # install
+        return 0
+      elif [[ $answer = [nN] ]]; then
+        # skip
+        print::message blue "$TARGET_PATH/$target_file skipped!"
+        return 1
       fi
     done
   fi
+}
 
-  # already exists a file at the same path
-  if [ -e "$INSTALLDIR/.$file" ] && [ -z "$FORCE" ]; then
+install::diff() {
+  local source_file="$1"
+  local target_file="$2"
+
+  if [ -e "$TARGET_PATH/$target_file" ] && [ -z "$FORCE" ]; then
     # checksum
-    md5_dotfiles=$(md5sum "$current_path/$file" | awk '{ print $1 }')
-    md5_home=$(md5sum "$INSTALLDIR/.$file" | awk '{ print $1 }')
+    local source_md5=$(md5sum "$SOURCE_PATH/$source_file" | awk '{ print $1 }')
+    local target_md5=$(md5sum "$TARGET_PATH/$target_file" | awk '{ print $1 }')
 
     # only if are different
-    if [ "$md5_dotfiles" != "$md5_home" ]; then
+    if [ "$source_md5" != "$target_md5" ]; then
       while true; do
-        read -e -p $'\033[0;33m'"$INSTALLDIR/.$file exists and is different, overwrite [y,n,d,h]?: "$'\033[0m' -n 1 answer
+        read -e -p $'\033[0;33m'"$TARGET_PATH/$target_file exists and is different, overwrite [y,n,d,h]?: "$'\033[0m' -n 1 answer
 
-        # print help
         if [[ $answer = [hH] ]]; then
+          # print help
           echo -e "y - overwrite"
           echo -e "n - not overwrite"
           echo -e "d - view diff"
           echo -e "h - print this help"
-          continue
-        fi
-
-        # print diff
-        if [[ $answer = [dD] ]]; then
-          git diff "$INSTALLDIR/.$file" "$current_path/$file"
-          continue
-        fi
-
-        # overwrite
-        if [[ $answer = [yY] ]]; then
-          break
-        fi
-
-        # skip
-        if [[ $answer = [nN] ]]; then
-          echo -e "$BLUE$INSTALLDIR/.$file skipped!$OFF"
-          continue 2
+        elif [[ $answer = [dD] ]]; then
+          # print diff
+          git diff "$TARGET_PATH/$target_file" "$SOURCE_PATH/$source_file"
+        elif [[ $answer = [yY] ]]; then
+          # overwrite
+          return 0
+        elif [[ $answer = [nN] ]]; then
+          # skip
+          print::message blue "$TARGET_PATH/$target_file skipped!"
+          return 1
         fi
       done
     else
       # skip because are equal
-      echo -e "$BLUE$INSTALLDIR/.$file skipped! (is equal)$OFF"
-      continue
+      print::message blue "$TARGET_PATH/$target_file skipped! (is equal)"
+      return 1
     fi
   fi
+}
+
+install::perform() {
+  local operation="$1"
+  local source_file="$2"
+  local target_file="$3"
 
   if [ -z "$PRETEND" ]; then
-    # copy the file
-    cp -f "$current_path/$file" "$INSTALLDIR/.$file"
-    echo -e "$GREEN$INSTALLDIR/.$file installed!$OFF"
+    file::$operation "$source_file" "$target_file"
   else
     # pretend
-    echo "[pretend] $current_path/$file -> $INSTALLDIR/.$file"
+    echo "[pretend] $SOURCE_PATH/$source_file -> $TARGET_PATH/$target_file"
   fi
-done 3< "$current_path/_files"
+}
+
+file::copy() {
+  local source_file="$1"
+  local target_file="$2"
+
+  cp --remove-destination "$SOURCE_PATH/$source_file" "$TARGET_PATH/$target_file"
+  print::message green "$SOURCE_PATH/$source_file copied to $TARGET_PATH/$target_file"
+}
+
+file::link() {
+  local source_file="$1"
+  local target_file="$2"
+
+  ln -s -f "$SOURCE_PATH/$source_file" "$TARGET_PATH/$target_file"
+  print::message green "$SOURCE_PATH/$source_file linked into $TARGET_PATH/$target_file"
+}
+
+check::dependencies
+options::parse "$@"
+process::dotfiles
